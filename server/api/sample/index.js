40a8b1b901942ec0adedeb6d1dfb4b904d2c4236
paulofs <pasouza@gmail.com>
2016-04-12 20:00:27 -0300

    sptreemap

--- server/api/sample/index.js
+++ server/api/sample/index.js
@@ -8,6 +8,7 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/search', controller.search);
 router.get("/spFullName", controller.spFullName);
+router.get("/spTreeMap", controller.spTreeMap);
 router.get('/:id', controller.show);
 router.get('spFullName/:spFullName', controller.showspFullName);
 router.post('/', controller.create);
--- server/api/sample/sample.controller.js
+++ server/api/sample/sample.controller.js
@@ -58,7 +58,7 @@ exports.spFullName = function (req, res) {
   filter = dot.dot(filter);
   console.log(filter);
   var matchQ = {};
-  
+
   for (var key in filter) {
     var regex = {};
     if (filter.hasOwnProperty(key)) {
@@ -69,18 +69,18 @@ exports.spFullName = function (req, res) {
       }
     }
   }
-  
-  console.log('MatchQ : ',matchQ);
+
+  console.log('MatchQ : ', matchQ);
   var aggregate = Sample.aggregate();
   aggregate
   //.match({"passport.biome": { $regex: /Caatinga/i}, "usecategory.who": { $regex: /DBI/i}})
-  .match(matchQ)
-  .group({_id: {genus: "$specieinfo.genus", specie: "$specieinfo.specie", author: "$specieinfo.authority"}, count: {"$sum" : 1}, species:{"$addToSet": "$_id"}})
-  .sort({_id: 1});
-  //console.log(aggregate);
+    .match(matchQ)
+    .group({ _id: { genus: "$specieinfo.genus", specie: "$specieinfo.specie", author: "$specieinfo.authority" }, count: { "$sum": 1 }, species: { "$addToSet": "$_id" } })
+    .sort({ _id: 1 });
+  console.log("AGGREGATE1: ", aggregate);
   var options = { page: req.query.page, limit: req.query.limit, sortBy: req.query.sort };
 
-  Sample.aggregatePaginate(aggregate, options , function (err, results, pageCount, itemCount) {
+  Sample.aggregatePaginate(aggregate, options, function (err, results, pageCount, itemCount) {
     if (err) { return handleError(res, err); }
     var data = {};
     data.samples = results;
@@ -91,6 +91,70 @@ exports.spFullName = function (req, res) {
   });
 };
 
+// Get a google chart formated treemap for filtered samples hierarchies by family/genus/specie+authority 
+exports.spTreeMap = function (req, res) {
+
+  var filter = JSON.parse(req.query.filter || '{}');
+  filter = dot.dot(filter);
+  console.log(filter);
+  var matchQ = {};
+
+  for (var key in filter) {
+    var regex = {};
+    if (filter.hasOwnProperty(key)) {
+      if (filter[key]) {
+        regex["$regex"] = new RegExp(filter[key], 'i');
+        //console.log( key, regex);      
+        matchQ[key] = regex;
+      }
+    }
+  }
+
+  console.log('MatchQ : ', matchQ);
+  var aggregate = Sample.aggregate(); 
+  
+  aggregate
+    //.match({"passport.biome": { $regex: /Caatinga/i}, "usecategory.who": { $regex: /DBI/i}})
+    .match(matchQ)
+    .group({ _id: { family: "$specieinfo.family", genus: "$specieinfo.genus", specie: "$specieinfo.specie", author: "$specieinfo.authority" }, count: { "$sum": 1 } });
+    //.sort({ _id: 1 });
+  console.log('AGGREGATE: ', aggregate._pipeline);
+  
+  
+  Sample.aggregate(aggregate._pipeline, function (err, results ) {
+    if (err) { return handleError(res, err); }
+    var treemap = {cols: [{id: 'id', label: 'ID', type: 'string'},
+         {id: 'parent', label: 'Parent', type: 'string'},
+         {id: 'size', label: 'Size', type: 'number'}
+         ],
+         rows: [{ c: [{ v: 'familyfake' }, { v: '#' }, { v: 0 }] }]
+    };
+    console.log(results);
+    console.log(treemap);
+    results.forEach( function (item) {
+      // family
+      console.log('item : ', item);
+      console.log(item._id.family);
+      console.log(treemap.rows);
+      //if (treemap.rows.lenght > 0) {
+        var findFamily = $.grep(treemap.rows, function (e) { return e.c[0].v === item._id.family });
+        console.log('ff :', findFamily);
+        if (findFamily.length == 0) {
+          var row = { c: [{ v: item._id.family }, { v: '#' }, { v: 0 }] };
+          treemap.rows.push(row);
+        } else {
+          console.log(findFamily[0]);
+        }
+      //}      
+    });
+    
+    data.samples = results;
+    //Todo - retornar o número exato de docs
+    //data.total = pageCount * req.query.limit;
+    console.log('Data : ', data);
+    return res.json(200, data);
+  });
+};
 
 // Get a single sample
 exports.show = function (req, res) {
@@ -103,7 +167,7 @@ exports.show = function (req, res) {
 
 // Get a single sample by spFullName (genus|specie|authority)
 exports.showspFullName = function (req, res) {
-  Sample.find({spFullName: req.params.spFullName}, function (err, sample) {
+  Sample.find({ spFullName: req.params.spFullName }, function (err, sample) {
     if (err) { return handleError(res, err); }
     if (!sample) { return res.send(404); }
     return res.json(sample);
